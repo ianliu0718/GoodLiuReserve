c4a93fe0743fd968e27182fb24b534cdc9a67290
ian.liu <ian.liu@presco.ws>
2021-09-12 01:04:31 +0800

    測試發送訊息

---
+++ script.js
@@ -0,0 +1,39 @@
+function initializeLiff(myLiffId) {
+    liff
+        .init({
+            liffId: myLiffId
+        })
+        .then(() => {
+            initializeApp();
+        })
+        .catch((err) => {
+            alert('啟動失敗。');
+        });
+  }
+
+  function initializeApp() {
+  var h = document.getElementById('result');
+  h.innerHTML = window.location.href;
+  //h.innerHTML = 'isLoggedIn：' + liff.isLoggedIn();
+  liff.login({ redirectUri: 'https://ianliu0718.github.io/GoodLiuReserve' });
+  //liff.login();
+  //h.innerHTML = 'isLoggedIn：' + liff.isLoggedIn();
+  // 登入
+  //liff.login({
+  //  // 使用者登入後要去到哪個頁面
+  //  redirectUri: 'https://www.xxxxxxx.com.tw'
+  //});
+
+
+  h.innerHTML = 'isLoggedIn：' + liff.isLoggedIn();
+liff.sendMessages([
+  {
+    type: 'text',
+    text: 'Hello, World!'
+  }
+])
+
+  }
+  //document.getElementById('result').innerHTML = 'isLoggedIn：' + liff.isLoggedIn();
+  //使用 LIFF_ID 初始化 LIFF 應用
+  //initializeLiff('1656397971-q9WB8y1b');
